e6e2c84828a2f70c6e1412bb197ef6e103186a7b
Thiago Diniz <thiago@eventick.com.br>
2015-08-04 00:51:03 -0300

    trying to fix index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php
+?php
+
 /*
  * This is an example file for a public interface and a bookmarklet. It
  * is provided so you can build from it and customize to suit your needs.
@@ -13,34 +14,34 @@
 require_once( dirname(__FILE__).'/includes/load-yourls.php' );
 
 // Change this to match the URL of your public interface. Something like: http://your-own-domain-here.com/index.php
-$page = YOURLS_SITE . '/index.php' ;
+$page = YOURLS_SITE . '/sample-public-front-page.php' ;
 
 // Part to be executed if FORM has been submitted
 if ( isset( $_REQUEST['url'] ) && $_REQUEST['url'] != 'http://' ) {
 
-        // Get parameters -- they will all be sanitized in yourls_add_new_link()
-        $url     = $_REQUEST['url'];
-        $keyword = isset( $_REQUEST['keyword'] ) ? $_REQUEST['keyword'] : '' ;
-        $title   = isset( $_REQUEST['title'] ) ?  $_REQUEST['title'] : '' ;
-        $text    = isset( $_REQUEST['text'] ) ?  $_REQUEST['text'] : '' ;
+	// Get parameters -- they will all be sanitized in yourls_add_new_link()
+	$url     = $_REQUEST['url'];
+	$keyword = isset( $_REQUEST['keyword'] ) ? $_REQUEST['keyword'] : '' ;
+	$title   = isset( $_REQUEST['title'] ) ?  $_REQUEST['title'] : '' ;
+	$text    = isset( $_REQUEST['text'] ) ?  $_REQUEST['text'] : '' ;
 
-        // Create short URL, receive array $return with various information
-        $return  = yourls_add_new_link( $url, $keyword, $title );
+	// Create short URL, receive array $return with various information
+	$return  = yourls_add_new_link( $url, $keyword, $title );
 
-        $shorturl = isset( $return['shorturl'] ) ? $return['shorturl'] : '';
-        $message  = isset( $return['message'] ) ? $return['message'] : '';
-        $title    = isset( $return['title'] ) ? $return['title'] : '';
-        $status   = isset( $return['status'] ) ? $return['status'] : '';
+	$shorturl = isset( $return['shorturl'] ) ? $return['shorturl'] : '';
+	$message  = isset( $return['message'] ) ? $return['message'] : '';
+	$title    = isset( $return['title'] ) ? $return['title'] : '';
+	$status   = isset( $return['status'] ) ? $return['status'] : '';
 
-        // Stop here if bookmarklet with a JSON callback function ("instant" bookmarklets)
-        if( isset( $_GET['jsonp'] ) && $_GET['jsonp'] == 'yourls' ) {
-                $short = $return['shorturl'] ? $return['shorturl'] : '';
-                $message = "Short URL (Ctrl+C to copy)";
-                header('Content-type: application/json');
-                echo yourls_apply_filter( 'bookmarklet_jsonp', "yourls_callback({'short_url':'$short','message':'$message'});" );
+	// Stop here if bookmarklet with a JSON callback function ("instant" bookmarklets)
+	if( isset( $_GET['jsonp'] ) && $_GET['jsonp'] == 'yourls' ) {
+		$short = $return['shorturl'] ? $return['shorturl'] : '';
+		$message = "Short URL (Ctrl+C to copy)";
+		header('Content-type: application/json');
+		echo yourls_apply_filter( 'bookmarklet_jsonp', "yourls_callback({'short_url':'$short','message':'$message'});" );
 
-                die();
-        }
+		die();
+	}
 }
 
 // Insert <head> markup and all CSS & JS files
@@ -54,78 +55,39 @@ yourls_html_menu() ;
 
 // Part to be executed if FORM has been submitted
 if ( isset( $_REQUEST['url'] ) && $_REQUEST['url'] != 'http://' ) {
-  // Part to be executed if FORM has been submitted
-if ( isset( $_REQUEST['url'] ) && $_REQUEST['url'] != 'http://' ) {
 
-        // Display result message of short link creation
-        if( isset( $message ) ) {
-                echo "<h2>$message</h2>";
-        }
+	// Display result message of short link creation
+	if( isset( $message ) ) {
+		echo "<h2>$message</h2>";
+	}
 
-        if( $status == 'success' ) {
-                // Include the Copy box and the Quick Share box
-                yourls_share_box( $url, $shorturl, $title, $text );
+	if( $status == 'success' ) {
+		// Include the Copy box and the Quick Share box
+		yourls_share_box( $url, $shorturl, $title, $text );
 
-                // Initialize clipboard -- requires js/share.js and js/jquery.zclip.min.js to be properly loaded in the <head>
-                echo "<script>init_clipboard();</script>\n";
-        }
+		// Initialize clipboard -- requires js/share.js and js/jquery.zclip.min.js to be properly loaded in the <head>
+		echo "<script>init_clipboard();</script>\n";
+	}
 
 // Part to be executed when no form has been submitted
 } else {
 
-                $site = YOURLS_SITE;
+		$site = YOURLS_SITE;
 
-                // Display the form
-                echo <<<HTML
-                <h2>Enter a new URL to shorten</h2>
-                <form method="post" action="">
-                <p><label>URL: <input type="text" class="text" name="url" value="http://" /></label></p>
-                <p><label>Optional custom short URL: $site/<input type="text" class="text" name="keyword" /></label></p>
-                <p><label>Optional title: <input type="text" class="text" name="title" /></label></p>
-                <p><input type="submit" class="button primary" value="Shorten" /></p>
-                </form>
+		// Display the form
+		echo <<<HTML
+		<h2>Enter a new URL to shorten</h2>
+		<form method="post" action="">
+		<p><label>URL: <input type="text" class="text" name="url" value="http://" /></label></p>
+		<p><label>Optional custom short URL: $site/<input type="text" class="text" name="keyword" /></label></p>
+		<p><label>Optional title: <input type="text" class="text" name="title" /></label></p>
+		<p><input type="submit" class="button primary" value="Shorten" /></p>
+		</form>
 HTML;
 
 }
 
 ?>
-
-
-
-<h2>Links</h2>
-<?php
-$table_url = YOURLS_DB_TABLE_URL;
-$where = '';
-
-yourls_table_head();
-yourls_table_tbody_start();
-
-// Main Query
-$where = yourls_apply_filter( 'admin_list_where', $where );
-$url_results = $ydb->get_results( "SELECT * FROM `$table_url` WHERE 1=1 $where ORDER BY `timestamp` DESC;" );
-$found_rows = false;
-
-if( $url_results ) {
-        $found_rows = true;
-        foreach( $url_results as $url_result ) {
-                $keyword = yourls_sanitize_string( $url_result->keyword );
-                $timestamp = strtotime( $url_result->timestamp );
-                $url = stripslashes( $url_result->url );
-                $ip = $url_result->ip;
-                $title = $url_result->title ? $url_result->title : '';
-                $clicks = $url_result->clicks;
-
-                echo yourls_table_add_row( $keyword, $url, $title, $ip, $clicks, $timestamp );
-         }
-}
-
-$display = $found_rows ? 'display:none' : '';
-echo '<tr id="nourl_found" style="'.$display.'"><td colspan="6">' . yourls__('No URL') . '</td></tr>';
-
-yourls_table_tbody_end();
-yourls_table_end();
-?>
-
 
 <h2>Bookmarklets</h2>
 
@@ -146,3 +108,8 @@ yourls_table_end();
 <h2>Please note</h2>
 
 <p>Be aware that a public interface <strong>will</strong> attract spammers. You are strongly advised to install anti spam plugins and any appropriate counter measure to deal with this issue.</p>
+
+<?php
+
+// Display page footer
+yourls_html_footer();	
